99506066745093327a2f7f3d079015995ff34cb5
bobbyhadz <opmftw@gmail.com>
2024-04-11 08:26:56 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# How to Wait for subprocess(es) to finish in Python
+
+import subprocess
+
+p1 = subprocess.Popen(['echo', 'bobbyhadz.com'])
+p2 = subprocess.Popen(['echo', 'google.com'])
+
+
+exit_codes = [p.wait() for p in (p1, p2)]
+
+print(exit_codes)
